774ba4fbf0273d0be1919f48a9c244533b2858d1
Julian <julian.schneider-gb@gmx.de>
2017-11-30 11:27:46 +0100

    changed console output of news from description to title

--- scrabNews.py
+++ scrabNews.py
@@ -2,14 +2,14 @@ import json
 import requests
 
 #request news from news-api at the moment from bild
-news = requests.get("https://newsapi.org/v1/articles?source=bild&sortBy=top&apiKey=x")
+news = requests.get("https://newsapi.org/v2/top-headlines?sources=bild&apiKey=x")
 news = news.json() #convert response into a json
 
 
 #debug json list elements
 i = 0
 for objects in news:
-    print(news["articles"][i]["description"])
+    print(news["articles"][i]["title"])
     i = i+1
 
 
